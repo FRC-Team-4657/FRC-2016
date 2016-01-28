d5c878be97d9072e49f0045c4f3f46721e4515d3
unknown <frcuser@J5J9QN1D.scfschools.com>
2016-01-28 10:56:55 -0600

    Created the .gitignore and added the Robot Project for the FRC 2016 season. The project has the Tank drive implementation worked out and the scissor lift has also been fully implemented. The driver is also now able to choose between 4 speeds for the scissor lift.

---
+++ Robot Project/src/org/usfirst/frc/team4657/robot/Robot.java
@@ -0,0 +1,113 @@
+
+package org.usfirst.frc.team4657.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import edu.wpi.first.wpilibj.Talon;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+
+    final Double defaultSpeed = 0.5;
+    final Double slowerSpeed = 0.25;
+    final Double fasterSpeed = 0.75;
+    final Double fastestSpeed = 1.0;
+    Double speedSelected;
+    SendableChooser chooserSpeed;
+
+    
+    RobotDrive myRobot;
+    Joystick stick1;
+    Joystick stick2;
+
+    Talon screwTalon = new Talon(2);
+
+    
+	
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {    
+        myRobot = new RobotDrive(0,1);
+    	stick1 = new Joystick(0);
+    	stick2 = new Joystick(1);
+    	
+    }
+    
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
+	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 */
+    public void autonomousInit() {
+
+    }
+    	
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+  
+    	
+    }
+    
+    public void telepoInit() {
+        chooserSpeed.addDefault("Default Speed : 0.5", defaultSpeed);
+        chooserSpeed.addObject("Speed Option : 0.25", slowerSpeed);
+        chooserSpeed.addObject("Speed Option : 0.75", fasterSpeed);
+        chooserSpeed.addObject("Speed Option : 1.0", fastestSpeed);
+        
+        //speedSelected = (Double) chooserSpeed.getSelected();
+        //System.out.println("Speed Selected: " + Double.toString(speedSelected));
+        
+        
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+    	myRobot.tankDrive(stick1, stick2);
+    	
+    		
+
+    	speedSelected = (Double) chooserSpeed.getSelected();
+        SmartDashboard.putNumber("Speed Selected: ", speedSelected);
+    	SmartDashboard.putBoolean("Left Trigger Enagaged: ", stick1.getTrigger());
+    	SmartDashboard.putBoolean("Right Trigger Enagaged: ", stick2.getTrigger());
+    	
+    	
+    	
+    	if (stick1.getTrigger() == true){
+    		screwTalon.set(speedSelected);
+    	}
+    	else if (stick2.getTrigger() == true){
+    		screwTalon.set(-speedSelected);
+    	}
+    	
+    }
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    
+    }
+    
+}
